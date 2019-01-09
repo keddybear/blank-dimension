3b979c4c790e64adb9270a3872d3ba826b12fb00
keddybear <xiong.49@buckeyemail.osu.edu>
2019-01-09 11:15:14 -0500

    Added get /* in express

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,18 @@
 require('dotenv').config();
 
 const express = require('express');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 8000;
+
+app.get('/*', (req, res) => {
+	res.sendFile(path.resolve('dist/index.html'), (err) => {
+		if (err) {
+			res.status(500).send(err);
+		}
+	});
+});
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
 
